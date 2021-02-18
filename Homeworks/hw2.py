72d9485aff0b7d2461fc47bfc264ce67fac0d126
sukranhan <51482047+sukranhan@users.noreply.github.com>
2021-02-18 16:07:02 +0300

    Add files via upload

---
+++ Homeworks/hw2.py
@@ -0,0 +1,32 @@
+grades=[]
+names=[]
+averages=[]
+for x in range(5):
+    temporary=[]
+    name=input('please enter your name and surname: ')
+    midterm=int(input('please enter your midterm grade :'))
+    final=int(input('please neter your final grade: '))
+    homework=int(input('please enter your homework grade: '))
+    temporary.insert(0,midterm)
+    temporary.insert(1,final)
+    temporary.insert(2,homework)
+    grades.append(temporary)
+    names.append(name)
+dictin={names[i]:grades[i] for i in range(len(names))}
+for i in range(len(grades)):
+    add=0
+    ave=0
+    for t in(grades[i]):
+        add+=t
+    ave=add/3
+    averages.append(ave)
+averages.sort()
+averages.reverse()
+print('Congratilation 'dictin.keys(
+print(dictin)
+print(averages)
+
+
+
+
+                
